10667ce3744162d666275f18985092005001733a
Anna Jones <annajones314@Annas-MacBook-Air.local>
2023-01-17 15:07:49 -0600

    created Xcode project

---
+++ iOS/RhodesEventApp/RhodesEventAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RhodesEventAppApp.swift
+//  RhodesEventApp
+//
+//  Created by Anna Jones on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RhodesEventAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
