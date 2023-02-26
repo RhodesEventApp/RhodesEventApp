3996f0abcc1f48fb4e92c9f0a6448f6242beebeb
Chris Hoang <hoadh-23@rhodes.edu>
2023-02-26 06:07:16 -0600

    update

--- app/platforms/android/app/src/main/assets/www/tabs/tabs.js
+++
@@ -1,4 +0,0 @@
-/*jslint browser: true */
-/*global aero */
-
-aero.tab.init();
--- app/platforms/browser/www/home/home.js
+++
@@ -1,7 +0,0 @@
-/*jslint browser: true */
-/*global aero */
-
-aero.touchclick(document.getElementById("exampleDialog"), function () {
-    "use strict";
-    aero.showDialog(document.querySelector("div.dialog"));
-});
